fc47ca189465b6abfd49157153fa7ba4d0a0003d
Eduardo Oliveira <eduardo.mrn.oliveira@outlook.com>
2026-06-06 20:40:46 -0300

    feat: set up pt-br locale

--- ProjetoFinal/src/app/app.config.ts
+++ ProjetoFinal/src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
+
 export const appConfig: ApplicationConfig = {
-	providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+	providers: [
+		provideBrowserGlobalErrorListeners(),
+		provideRouter(routes),
+		{ provide: LOCALE_ID, useValue: 'pt-BR' },
+	],
 };
