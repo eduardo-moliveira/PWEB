d4b97091413d8fa6b946846a83bbcd92f0e42f99
Eduardo Oliveira <eduardo.mrn.oliveira@outlook.com>
2026-06-06 21:33:48 -0300

    feat: implement dashboard layout and integrate sub-components

--- ProjetoFinal/src/app/pages/dashboard/dashboard.ts
+++ ProjetoFinal/src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { ScheduleComponent } from './components/schedule/schedule';
+import { AppointmentService } from '../../shared/services/appointment/appointment';
+import { AppointmentListSectionComponent } from './components/appointment-list-section/appointment-list-section';
+import { UnconfirmedAppointmentCardComponent } from './components/unconfirmed-appointment-card/unconfirmed-appointment-card';
+import { UnpaidAppointmentCardComponent } from './components/unpaid-appointment-card/unpaid-appointment-card';
 
 @Component({
 	selector: 'app-dashboard',
-	imports: [],
+	imports: [
+		DatePipe,
+		ScheduleComponent,
+		AppointmentListSectionComponent,
+		UnconfirmedAppointmentCardComponent,
+		UnpaidAppointmentCardComponent,
+	],
 	templateUrl: './dashboard.html',
 	styleUrl: './dashboard.scss',
 })
-export class Dashboard {}
+export class Dashboard implements OnInit {
+	private _appointmentService = inject(AppointmentService);
+
+	currentDate = new Date();
+
+	confirmedAppointments = this._appointmentService.confirmedAppointments;
+	unconfirmedAppointments = this._appointmentService.unconfirmedAppointments;
+	unpaidAppointments = this._appointmentService.unpaidAppointments;
+
+	ngOnInit() {
+		this._appointmentService.fetchAll();
+	}
+}
